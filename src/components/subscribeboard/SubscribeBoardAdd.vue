c8068e806f3f8d61b8d349255182bda172850aa5
SkylarLee1003 <skylarlee1003@gmail.com>
2023-06-16 17:56:41 +0900

    subscribeboard + 부트스트랩 +

---
+++ src/components/subscribeboard/SubscribeBoardAdd.vue
@@ -0,0 +1,38 @@
+<template lang="">
+    <div>
+        <h1> 글 등록 </h1>
+        <input type="text" name="email" v-model="email">
+        <input type="text" name="title" v-model="title">
+        <input type="text" name="site" v-model="site">
+        <input type="text" name="total_point" v-model="total_point">
+        <input type="text" name="total_people" v-model="total_people">
+        <!-- register date 는 sysdate -->
+        <input type="text" name="recruit_endperiod" v-model="recruit_endperiod">
+        <input type="text" name="payment_date" v-model="payment_date">
+        <input type="text" name="subscribe_startdate" v-model="subscribe_startdate">
+        <input type="text" name="subscribe_enddate" v-model="subscribe_enddate">
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'SubscribeBoardAdd',
+    data(){
+        return {
+            email: 'qwer',
+            title: '',
+            site: '',
+            total_point: 0,
+            total_people: 0,
+            register_date: null,
+            recruit_endperiod: null,
+            payment_date: null,
+            subscribe_startdate: null,
+            subscribe_enddate: null
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/subscribeboard/SubscribeBoardList.vue
@@ -0,0 +1,38 @@
+<template lang="">
+    <div>
+        <div class="container text-center">
+       
+            <div class="grid text-center">
+                <h1> 구독 공유 시작하기 </h1>
+            </div>
+            <div class="row"> 
+                <div class="col">넷플릭스</div>
+                <div class="col">왓챠</div>
+                <div class="col">티빙</div>
+                <div class="col">웨이브</div>
+                <div class="col-2">디즈니플러스</div>
+                <div class="col-2">프라임 비디오</div>
+            </div>
+        </div>  
+        <h3><router-link to="/SubscribeBoardAdd">글 등록</router-link>  </h3>
+    </div>
+    
+</template>
+<script>
+export default {
+    name: 'App',
+    components: {
+    }
+}
+</script>
+ 
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
