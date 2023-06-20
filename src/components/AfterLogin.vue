e2a40522357acccfd85d8af870cd6ea0c84d46e4
hennie-yun <hennie9401@gmail.com>
2023-06-20 14:02:58 +0900

    mainpage

---
+++ src/components/AfterLogin.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>로그인 후 페이지</h1>
+      <router-view></router-view>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AfterLogin',
+  }
+  </script>
+  
+  <style scoped>
+  </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-//로그인 페이지 보이게 homeview를 로그인 페이지로 설정 
-import Homeview from '../components/members/login.vue'
+import HomeView from '../views/HomeView.vue'
 import SubscribeBoardList from '../components/subscribeboard/SubscribeBoardList.vue'
 import SubscribeBoardAdd from '../components/subscribeboard/SubscribeBoardAdd.vue'
+import login from '../components/members/login.vue'
+import join from '../components/members/join.vue'
+import AfterLogin from '../components/AfterLogin.vue'
+import calendar from '../components/calendar.vue'
 
 const routes = [
-  // 제일처음 시작할때 나올 로그인 화면 
   {
-    path: '/',
-    name: 'home',
-    component: Homeview
+    path: "/",
+    redirect: "/login"
   },
   {
     path: "/login",
@@ -17,28 +18,41 @@ const routes = [
     component: login
   },
   {
-    path: '/calendar',
-    name: 'calendar',
-    component: Calendar
-  },
-  {
     path: "/join",
     name: 'join',
     component: join
   },
   {
-    path: '/SubscribeBoardAdd',
-    name: 'SubscribeBoardAdd',
-    component: SubscribeBoardAdd
-  },
-  {
-    path: '/SubscribeBoardList',
-    name: 'SubscribeBoardList',
-    component: SubscribeBoardList
+    path: "/afterlogin",
+    component: AfterLogin,
+    children: [
+      {
+        path: "",
+        component: HomeView,
+      },
+      {
+        path: '/calendar',
+        name: 'calendar',
+        component: calendar
+      },
+      {
+        path: '/SubscribeBoardAdd',
+        name: 'SubscribeBoardAdd',
+        component: SubscribeBoardAdd
+      },
+      {
+        path: '/SubscribeBoardList',
+        name: 'SubscribeBoardList',
+        component: SubscribeBoardList
+      }
+    ]
   }
-]
+];
+
+
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
 export default router
