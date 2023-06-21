6c7b365f4469eb4f786de8c298d101f8b902be60
SkylarLee1003 <skylarlee1003@gmail.com>
2023-06-21 11:10:36 +0900

    subboard 디테일 페이지subscribeparty추가, subparty디테일페이지

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Homeview from '../components/members/login.vue'
 import SubscribeBoardList from '../components/subscribeboard/SubscribeBoardList.vue'
 import SubscribeBoardAdd from '../components/subscribeboard/SubscribeBoardAdd.vue'
+import SubscribeBoardDetail from '../components/subscribeboard/SubscribeBoardDetail.vue'
 import Calendar from '../components/calendars/calendar'
 import login from '../components/members/login.vue'
 import join from '../components/members/join.vue'
+import test from '../components/subscribeboard/test.vue'
 
 const routes = [
   // 제일처음 시작할때 나올 로그인 화면 
@@ -13,6 +15,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Homeview
+  },
+  {
+    path: '/test',
+    name:'test',
+    component: test
   },
   {
     path: "/login",
@@ -38,6 +45,11 @@ const routes = [
     path: '/SubscribeBoardList',
     name: 'SubscribeBoardList',
     component: SubscribeBoardList
+  },
+  {
+    path: '/SubscribeBoardDetail',
+    name: 'SubscribeBoardDetail',
+    component: SubscribeBoardDetail
   }
 ]
 const router = createRouter({
