d9343c71b14d197f06e6da579e868d8a330128dc
rmsgml7553 <rmsgml7553@naver.com>
2023-06-27 17:30:18 +0900

    naver api

--- src/components/groups/naver.vue
+++ src/components/groups/naver.vue
@@ -1,44 +1,46 @@
 <template>
-    <div>
-        <div>hello</div>
-      <button @click="signInWithNaver">Sign In with Naver</button>
-    
-    </div>
-  </template>
-  
-  <script>
-  export default {
-   mounted() {
-    const script = document.createElement('script')
-    script.src = 'https://static.nid.naver.com/js/naverLogin_implicit-1.0.3.js'
-    script.async = true
-    script.charset = 'utf-8'
-    document.head.appendChild(script)
-    
-    script.onload = () => {
-    const naver_id_login = new window.naver_id_login("IiiFJKBOyzL3qvfXasPq", "http://localhost:8181/api/naver/callback")
-    alert(naver_id_login.oauthParams.access_token)
-    this.$axios.get('http://localhost:8181/api/naver/accesstoken', {headers:{'access_token':naver_id_login.oauthParams.access_token}})
-     .then(function(res){
+  <div>
 
-        if(res.status == 200){
-            alert(res.data.flag)
-        } else {
-            alert(res.data.flag)
-        }
-     })
+    <button @click="authorize">Authorize with Naver</button>
+  </div>
+</template> 
+
+<script>
+import axios from 'axios';
+
+export default {
+  data(){
+    return{
+
     }
-   
-},
-name: 'naver',
-methods: {
-    signInWithNaver(){
-        const naver_id_login = new window.naver_id_login("IiiFJKBOyzL3qvfXasPq", "http://localhost:8181/api/naver/callback")
-        alert(naver_id_login.oauthParams.access_token)
-    }
-},
-  };
-  </script>
+  },
+  created:function(){ 
+
+
+  },
+ 
+  methods: {
+ 
+  authorize(){
+    let clientId = "IiiFJKBOyzL3qvfXasPq"
+    let clientSecret = "PtvyRoMmt_"
+    const self = this
+    self.$axios.get("http://localhost:8181/api/naver/go")
+    .then(function(res){
+      if(res.status == 200){
+        console.log(res.data)
+      } else {
+        alert(res.status)
+      }
+    })
+    location.href="http://localhost:8181/api/naver/login"
+  }
+  }
+}   
+    
+
+</script>
+
 <style lang="">
     
 </style>
